88b20d25e1f5f673de28b20646b59957845d19d8
Aditya Govardhan <dtgovardhan@gmail.com>
2020-05-29 03:51:59 -0700

    dao layer WIP, async func chaining critical

--- src/users/UserPersistence.js
+++ src/users/UserPersistence.js
@@ -1,40 +1,73 @@
 const { MongoClient } = require('mongodb');
 
-const url = 'mongodb://localhost:27017';
+// const url = 'mongodb://localhost:27017';
 
-const dbName = 'myproject';
+// const dbName = 'handicrafts-platform';
 
-const client = new MongoClient(url, { useUnifiedTopology: true });
+// const client = new MongoClient(url, { useUnifiedTopology: true });
 
-const insertDocuments = (db, callback) => {
-  const collection = db.collection('documents');
+const connectToDb = () => {
+  const url = 'mongodb://localhost:27017';
+  const dbName = 'handicrafts-platform';
+  const client = new MongoClient(url, { useUnifiedTopology: true });
 
-  collection.insertMany([
-    { a: 1 }, { a: 2 }, { a: 3 },
-  ], (err, result) => {
-    console.log(result);
-    console.log('Inserted 3 documents into the collection');
+  client.connect(() => {
+    console.log('Connected successfully to the DB');
+  });
+
+  const db = client.db(dbName);
+  return db;
+};
+
+const insertUser = (db, user, callback) => {
+  const collection = db.collection('users');
+
+  collection.insertOne(user, (err, result) => {
+    if (err) console.log(err);
     callback(result);
   });
 };
 
-const findDocuments = (db, callback) => {
-  const collection = db.collection('documents');
+const findUser = (db, searchParam, callback) => {
+  const collection = db.collection('users');
 
-  collection.find({ a: 3 }).toArray((err, docs) => {
-    console.log('Found the following records');
-    console.log(docs);
-    callback(docs);
+  collection.find(searchParam).toArray((err, result) => {
+    if (err) console.log(err);
+    callback(result);
   });
 };
 
-client.connect(() => {
-  console.log('Connected successfully to the DB');
-  const db = client.db(dbName);
+module.exports = { connectToDb, insertUser, findUser };
 
-  insertDocuments(db, () => {
-    findDocuments(db, () => {
-      client.close();
-    });
-  });
-});
+// const insertDocuments = (db, callback) => {
+//   const collection = db.collection('users');
+
+//   collection.insertMany([
+//     { a: 1, b: 1 }, { a: 2, b: 2 }, { a: 3, b: 3 },
+//   ], (err, result) => {
+//     console.log(result);
+//     console.log('Inserted 3 documents into the collection');
+//     callback(result);
+//   });
+// };
+
+// const findDocuments = (db, callback) => {
+//   const collection = db.collection('users');
+
+//   collection.find({ c: 3 }).toArray((err, docs) => {
+//     console.log('Found the following records');
+//     console.log(docs);
+//     callback(docs);
+//   });
+// };
+
+// client.connect(() => {
+//   console.log('Connected successfully to the DB');
+//   const db = client.db(dbName);
+
+//   insertDocuments(db, () => {
+//     findDocuments(db, () => {
+//       client.close();
+//     });
+//   });
+// });
