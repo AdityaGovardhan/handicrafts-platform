d888607897ecba649a855c63e0eea440d24b50bd
Aditya Govardhan <dtgovardhan@gmail.com>
2020-05-30 14:16:49 -0700

    serve static content

--- src/server.js
+++ src/server.js
@@ -5,10 +5,11 @@ const usersRouter = require('./users');
 
 const mainApp = express();
 
-const PORT = 3001; // take from config later
+const PORT = 3001; // TODO: take from config later
 
 mainApp.get('/', (req, res) => res.send('main page'));
 
+mainApp.use('/static', express.static('public/assets'));
 mainApp.use('/', itemsRouter);
 mainApp.use('/', usersRouter);
 
